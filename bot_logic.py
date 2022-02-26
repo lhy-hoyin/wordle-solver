e25cb7774aab0109244306d063493eee7d42f217
lhy-hoyin <88828097+lhy-hoyin@users.noreply.github.com>
2022-02-26 22:39:18 +0800

    Added bot logic responses

--- bot_logic.py
+++ bot_logic.py
@@ -1,4 +1,12 @@
 def bot_respond(input_text):
-    text = input_text.lower()
+    input_text = input_text.lower() # Change to lower characters
+    input_text = input_text.strip() # remove leading/ending characters
+    
+    if ' ' in input_text:
+        return "Sorry! You can only enter 1 word :("
+    
+    if len(input_text) != 5:
+        return "You need to enter a 5-letter word"
+    
     print("responding")
     return "hello"
