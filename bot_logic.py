e0a2f0f38ccba4fb47e9f4ff3b0043e8c6ea330f
lhy-hoyin <88828097+lhy-hoyin@users.noreply.github.com>
2022-02-26 22:29:21 +0800

    Code cleanup

---
+++ bot_logic.py
@@ -0,0 +1,4 @@
+def bot_respond(input_text):
+    text = input_text.lower()
+    print("responding")
+    return "hello"
--- wordlebot.py
+++ wordlebot.py
@@ -1,20 +1,17 @@
 from telegram.ext import *
 #from telegram import *      #for inlinekeyboard
 
-import responseTest as R
-#import teleKey as key
+from bot_logic import bot_respond
 
 def start_cmd(update, context):
     update.message.reply_text('Hello, did you need help with Wordle?')
-    #add Inline keyboard for Yes and Exit button
+    # TODO: add inline keyboard for Yes and Exit button
 
 def help_cmd(update, context):
     update.message.reply_text('Ask google for help, we only had 24 hours for this')
 
 def handle_msg(update, context):
-    text = str(update.message.text).lower()
-
-    response = R.sample_responses(text)
+    response = bot_respond(update.message.text)
     update.message.reply_text(response)
 
 def error(update, context):
@@ -25,14 +22,13 @@ def main():
         BOT_TOKEN = token.read()
     
     updater = Updater(BOT_TOKEN, use_context=True)
-    
     dpc = updater.dispatcher
 
     dpc.add_handler(CommandHandler("start", start_cmd))
     dpc.add_handler(CommandHandler("help", help_cmd))
-
+    
     dpc.add_handler(MessageHandler(Filters.text, handle_msg))
-
+    
     dpc.add_error_handler(error)
     
     updater.start_polling()
