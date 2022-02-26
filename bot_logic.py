ad3e7b9c36047980734dd0a899519052d1caa6fb
lhy-hoyin <88828097+lhy-hoyin@users.noreply.github.com>
2022-02-27 00:41:09 +0800

    Added more bot logic, Interfaced bot logic with wordle solver

--- bot_logic.py
+++ bot_logic.py
@@ -1,12 +1,90 @@
-def bot_respond(input_text):
-    input_text = input_text.lower() # Change to lower characters
-    input_text = input_text.strip() # remove leading/ending characters
+from wordle_solver import WORDLE_WORD_LENGTH, status_help_message, wordle_solver
+
+class bot_logic:
+
+    DEFAULT_SUGGEST_N_WORDS = 10
+
+    flag_enter_word = True
+    flag_enter_result = False
+    entered_word, entered_result = "", ""
+    possible_words = {}
     
-    if ' ' in input_text:
-        return "Sorry! You can only enter 1 word :("
+    def __init__(self):
+        self.solver = wordle_solver()
     
-    if len(input_text) != 5:
-        return "You need to enter a 5-letter word"
+    def respond(self, input_text):
+        if input_text.lower() in ("hi", "hey", "yoyoyo"):
+            return "Hello"
+        
+        input_text = input_text.lower() # Change to lower characters
+        input_text = input_text.strip() # remove leading/ending characters
+        
+        if ' ' in input_text:
+            return "Oops! Looks like you entered more than 1 word"
+        
+        if len(input_text) != WORDLE_WORD_LENGTH:
+            return "Oh no! You need to enter a " + str(WORDLE_WORD_LENGTH) + "-letter word"
+        
+        if self.flag_enter_word:
+            if not input_text.isalpha():
+                return "This doesn't look like a word :("
+            self.entered_word = input_text
+            self.toggle_status()
+            return "Now enter the word status returned by Wordle"
+        
+        elif self.flag_enter_result:
+            if not input_text.isdecimal():
+                return "Hmm...I don't quite understand this format :(\nType /format for more info" 
+                # maybe use inline kb here(??)
+            #fixme: code supposed to check that no other digits are input
+            # except for 0, 1, 2
+            '''
+            if not any((d in set("012")) for d in input_text):
+                return "wrong digit(s)"
+            '''
+            if input_text == "11111":
+                return "Yay!!! You got the correct answer already!\n(Hint: type /restart to try the next one)"
+            
+            self.entered_result = input_text
+            return self.run_solver()
+        
+        return "default-response"
+       
+    def run_solver(self):
+        assert len(self.entered_word) != 0
+        assert len(self.entered_result) != 0
+        self.possible_words = self.solver.try_word({ self.entered_word : self.entered_result })
+        self.toggle_status()
+        return self.suggest_words()
     
-    print("responding")
-    return "hello"
+    def suggest_words(self, num = DEFAULT_SUGGEST_N_WORDS):
+        # Retrieve suggestions from dict
+        suggestions = list(self.possible_words.items())[:num]
+                
+        reply = "I suggest trying " + suggestions[0][0] + "\n"
+        reply += "You can also try:"
+        
+        for word_pair in suggestions:
+            if word_pair !=  suggestions[0]:
+                reply += "\n" + word_pair[0]
+        
+        #TODO: can also display numbers to users (to make a better choice)
+        return reply
+    
+    def restart(self):
+        self.solver = wordle_solver()
+        self.entered_word = ""
+        self.entered_result = ""
+        self.possible_words = {}
+    
+    def result_format_message_str(self):
+        return status_help_message()
+    
+    def toggle_status(self):
+        self.flag_enter_word = not self.flag_enter_word
+        self.flag_enter_result = not self.flag_enter_result
+
+if __name__ == "__main__":
+    bot = bot_logic()
+    while(True):
+        print(bot.respond(input("> ")))
--- wordlebot.py
+++ wordlebot.py
@@ -1,17 +1,26 @@
 from telegram.ext import *
 #from telegram import *      #for inlinekeyboard
 
-from bot_logic import bot_respond
+from bot_logic import bot_logic
+
+user_bot = {}
 
 def start_cmd(update, context):
+    user_bot[str(update.effective_chat.id)] = bot_logic()
     update.message.reply_text('Hello, did you need help with Wordle?')
     # TODO: add inline keyboard for Yes and Exit button
 
 def help_cmd(update, context):
-    update.message.reply_text('Ask google for help, we only had 24 hours for this')
+    update.message.reply_text('Ask google for help, we only had 24 hours for this XD')
+
+def result_format_cmd(update, context):
+    update.message.reply_text(user_bot[str(update.effective_chat.id)].result_format_message_str())
+    
+def restart_cmd(update, context):
+    update.message.reply_text(user_bot[str(update.effective_chat.id)].restart())
 
 def handle_msg(update, context):
-    response = bot_respond(update.message.text)
+    response = user_bot[str(update.effective_chat.id)].respond(update.message.text)
     update.message.reply_text(response)
 
 def error(update, context):
@@ -26,6 +35,8 @@ def main():
 
     dpc.add_handler(CommandHandler("start", start_cmd))
     dpc.add_handler(CommandHandler("help", help_cmd))
+    dpc.add_handler(CommandHandler("format", result_format_cmd))
+    dpc.add_handler(CommandHandler("restart", restart_cmd))
     
     dpc.add_handler(MessageHandler(Filters.text, handle_msg))
     
