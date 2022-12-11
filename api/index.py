b9111ebd88bd860b88e72087149cf706fbdc119e
lhy-hoyin <88828097+lhy-hoyin@users.noreply.github.com>
2022-12-11 15:57:06 +0800

    no message

--- api/index.py
+++ api/index.py
@@ -1,17 +1,12 @@
 from http.server import BaseHTTPRequestHandler
 from urllib import parse
 
-
-
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.send_header('Content-type','text/html')
         self.end_headers()
         message="Hello World"
-        
-        START_PHOTO_PATH = '../img/wordle-solver-light.jpg'
-        
-        self.wfile.write(open(START_PHOTO_PATH))
+        self.wfile.write(open(join('img','wordle-solver-light.jpg')), 'rb')
         return
         
