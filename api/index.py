7ba7513a796907b76ba86c9b907d2e31ca556306
lhy-hoyin <88828097+lhy-hoyin@users.noreply.github.com>
2022-12-11 16:35:44 +0800

    changed header

--- api/index.py
+++ api/index.py
@@ -4,15 +4,13 @@ from urllib import parse
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
-        self.send_header('Content-type','text/html')
+        self.send_header('Content-type','image.jpeg')
         self.end_headers()
 
         self.wfile.write("hello world".encode())
 
         with open('test.jpg', 'rb') as f:
-            byte = f.read(1)
-            while byte:
-                self.wfile.write(byte)
+            self.wfile.write(f.read(-1))
                 
         return
         
