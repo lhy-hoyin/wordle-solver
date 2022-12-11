3d1adeff65519f908bfa1fce2e4d6c5a9d474be4
lhy-hoyin <88828097+lhy-hoyin@users.noreply.github.com>
2022-12-11 16:43:30 +0800

    no message

--- api/index.py
+++ api/index.py
@@ -1,5 +1,6 @@
 from http.server import BaseHTTPRequestHandler
 from urllib import parse
+import shutil
 
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
@@ -9,10 +10,13 @@ class handler(BaseHTTPRequestHandler):
 
         self.wfile.write("hello world".encode())
 
-        with open('test.jpg', 'rb') as f:
-            bytes=f.read(1)
-            while bytes:
-                self.wfile.write(bytes)
+        #with open('test.jpg', 'rb') as f:
+        #    bytes=f.read(1)
+        #    while bytes:
+        #        self.wfile.write(bytes)
+                
+        with open('test.jpg', 'rb') as content:
+            shutil.copyfileobj(content, self.wfile)
                 
         return
         
