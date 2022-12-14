b1200c9c029c3b7808e8f7e04fedb16911c53f27
lhy-hoyin <88828097+lhy-hoyin@users.noreply.github.com>
2022-12-14 16:37:52 +0800

    no message

--- api/index.py
+++ api/index.py
@@ -6,24 +6,7 @@ from solver.wordle_bot import main
 
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type','image/jpg')
-        self.end_headers()
-
-        self.wfile.write("hello world".encode())
-
-        #with open('test.jpg', 'rb') as f:
-        #    bytes=f.read(1)
-        #    while bytes:
-        #        self.wfile.write(bytes)
-                
-        with open('test.jpg', 'rb') as content:
-            shutil.copyfileobj(content, self.wfile)
-                
-        return
-        
-    def run():
-        main()
+        main()        
         
 if __name__ == "__main__":
     main()
